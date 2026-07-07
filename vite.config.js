f856cb2cc9c5b2aa69302f3e435a6392d04a584a
dalu <1300122933@qq.com>
2026-07-07 17:57:18 +0800

    fix: 新增压缩插件

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import { compression } from 'vite-plugin-compression2'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), compression()],
   server: {
     port: 5173,
     open: false
